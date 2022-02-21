14b783ae899baad13dcce201df3d4c11bbb20802
EdRomer <98138884+EdRomer@users.noreply.github.com>
2022-02-21 15:39:46 -0600

    Update Ejercicio1.sql

--- Actividad 2.2/Ejercicio1.sql
+++ Actividad 2.2/Ejercicio1.sql
@@ -1,10 +1,20 @@
 delimiter //
-CREATE PROCEDURE sp_guardar_subscriptor()
+CREATE PROCEDURE sp_guardar_subscriptor((
+	in p_idSubscriptor int,
+    in p_codSubscriptor int,
+    in p_nombSubscriptor varchar(25),
+    in p_apeSubscriptor varchar(25)
+)
 BEGIN
 	declare v_idSubscriptor int;
     declare v_codSubscriptor int;
     declare v_nombSubscriptor varchar(25);
     declare v_apeSubscriptor varchar(25);
+	
+    set v_idSubscriptor = p_idSubscriptor;
+    set v_codSubscriptor = p_codSubscriptor;
+    set v_nombSubscriptor = p_nombSubscriptor;
+    set v_apeSubscriptor = p_apeSubscriptor;
     
     
     update tbl_subscriptores
