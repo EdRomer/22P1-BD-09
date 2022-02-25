d4efffcdc58b953d38f53e05b1b76dcf0533f7e2
DaniloRiver <87211142+DaniloRiver@users.noreply.github.com>
2022-02-25 17:43:29 -0600

    Update Ejercicio4.sql

--- Actividad 2.2/Ejercicio4.sql
+++ Actividad 2.2/Ejercicio4.sql
@@ -20,7 +20,7 @@ BEGIN
     set v_precio_prod       = 0.0;
     
     
-    insert tbl_items_factura (id_factura,id_producto,cantidad)
+    insert bd_sample.tbl_items_factura (id_factura,id_producto,cantidad)
     values (v_id_factura,v_id_producto,v_cantidad);
     
     select sum(cantidad) into v_numero_items
@@ -31,12 +31,12 @@ BEGIN
     from bd_sample.tbl_productos 
     where id_producto = v_id_producto; 
 
-    update tbl_facturas
-    Set numero_item = v_numero_items,
+    update  bd_sample.tbl_facturas
+    Set numero_items = v_numero_items,
         isv_total   = (subtotal + v_precio_prod*v_cantidad)*0.15,
         subtotal    =  subtotal + v_precio_prod*v_cantidad,
 	totapagar=     (subtotal)*1.15
-    where id_factura=v_idFactura;
+    where id_factura = v_id_factura;
     
     commit;
     
